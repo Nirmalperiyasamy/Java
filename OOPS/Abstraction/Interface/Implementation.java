dab53e9e55cc2433189e157fb5e9dab05e3ed00b
Nirmal <105903253+Nirmalperiasamy@users.noreply.github.com>
2023-02-09 11:31:53 +0530

    Removed file

--- OOPS/Abstraction/Interface/Implementation.java
+++
@@ -1,8 +0,0 @@
-package OOPS.Abstraction.Interface;
-
-public class Implementation {
-    public static void main(String[] args) {
-        Circle person =new Circle();
-        person.draw();
-    }
-}
